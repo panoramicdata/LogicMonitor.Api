9c15d02c6d1a3e32508f3659527b3c73ae7b6e1a
rolandbanks <roland.banks@panoramicdata.com>
2020-05-05 15:29:00 +0700

    Up-versioned to 2.135.1 which fixed broken GetLogicModuleMetadata method.

--- LogicMonitor.Api/PortalClient_LogicModules.cs
+++ LogicMonitor.Api/PortalClient_LogicModules.cs
@@ -54,7 +54,7 @@ namespace LogicMonitor.Api
 			int? versionOverride = null,
 			CancellationToken cancellationToken = default)
 		{
-			versionOverride ??= 133; // TODO: use the actual version we want
+			versionOverride ??= 135; // TODO: use the actual version we want
 			var typeParameter = string.Empty;
 			switch (logicModuleType)
 			{
@@ -167,7 +167,7 @@ namespace LogicMonitor.Api
 			int? versionOverride = null,
 			CancellationToken cancellationToken = default)
 		{
-			versionOverride ??= 133; // TODO: use the actual version we want
+			versionOverride ??= 135; // TODO: use the actual version we want
 
 			string typeEndpoint;
 			switch (logicModuleType)
@@ -219,7 +219,7 @@ namespace LogicMonitor.Api
 			int? versionOverride = null,
 			CancellationToken cancellationToken = default)
 		{
-			versionOverride ??= 133; // TODO: use the actual version we want
+			versionOverride ??= 135; // TODO: use the actual version we want
 
 			await PostAsync<SnmpSysOidMapImportObject, object>
 			(
