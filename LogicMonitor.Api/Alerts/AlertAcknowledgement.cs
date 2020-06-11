2d82b5e8ce689dec1dbc29d95e0d9af299dfff68
Tom Dicks <a_fruitcake@hotmail.co.uk>
2020-06-11 22:19:54 +0100

    Fix for ackComment property
    Added attributes for the JsonSerializer to convert correctly.

--- LogicMonitor.Api/Alerts/AlertAcknowledgement.cs
+++ LogicMonitor.Api/Alerts/AlertAcknowledgement.cs
@@ -3,11 +3,13 @@ namespace LogicMonitor.Api.Alerts
 	/// <summary>
 	/// An alert acknowledgement
 	/// </summary>
+	[DataContract]
 	public class AlertAcknowledgement
 	{
 		/// <summary>
 		/// The acknowledgement comment
 		/// </summary>
+		[DataMember(Name="ackComment")]
 		public string AcknowledgementComment { get; set; }
 	}
 }
