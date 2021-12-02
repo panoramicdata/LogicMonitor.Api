5cf3b76c77f3bb6c23aa0af9d4d81fd6ce479cc2
David Bond <david.bond@panoramicdata.com>
2021-12-02 15:31:53 +0000

    Fixed NetscanCreationDto DDR Assignment Type

--- LogicMonitor.Api/Netscans/NetscanAssignmentType.cs
+++ LogicMonitor.Api/Netscans/NetscanAssignmentType.cs
@@ -4,7 +4,6 @@ namespace LogicMonitor.Api.Netscans;
 ///    A netscan policy assignment type
 /// </summary>
 [DataContract]
-[JsonConverter(typeof(StringEnumConverter))]
 public enum NetscanAssignmentType
 {
 	/// <summary>
@@ -16,36 +15,30 @@ public enum NetscanAssignmentType
 	/// <summary>
 	///    All devices
 	/// </summary>
-	[EnumMember(Value = "1")]
 	All = 1,
 
 	/// <summary>
 	///    Cisco devices
 	/// </summary>
-	[EnumMember(Value = "2")]
 	Cisco = 2,
 
 	/// <summary>
 	///    Linux devices
 	/// </summary>
-	[EnumMember(Value = "3")]
 	Linux = 3,
 
 	/// <summary>
 	///    NetApp devices
 	/// </summary>
-	[EnumMember(Value = "4")]
 	NetApp = 4,
 
 	/// <summary>
 	///    Windows devices
 	/// </summary>
-	[EnumMember(Value = "5")]
 	Windows = 5,
 
 	/// <summary>
 	///    Devices specified using a query string
 	/// </summary>
-	[EnumMember(Value = "6")]
 	Custom = 6
 }
