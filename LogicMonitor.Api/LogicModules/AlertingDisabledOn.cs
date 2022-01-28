047c2d3f74b2ae76997e7db449ddfae162d75e00
David Bond <david.bond@panoramicdata.com>
2022-01-28 17:00:07 +0000

    Temporarily downgraded the AlertingDisabledOn class

--- LogicMonitor.Api/LogicModules/AlertingDisabledOn.cs
+++ LogicMonitor.Api/LogicModules/AlertingDisabledOn.cs
@@ -2,25 +2,35 @@ namespace LogicMonitor.Api.LogicModules;
 
 /// <summary>
 /// An Alerting Disabled on class
+/// LogicMonitor sometimes return this as a string,
+/// so this must be handled as an object until LogicMonitor fix this
 /// </summary>
 [DataContract]
-public class AlertingDisabledOn : IdentifiedItem
+public class AlertingDisabledOn : object
 {
-	/// <summary>
-	/// The display name
-	/// </summary>
-	[DataMember(Name = "displayName")]
-	public string DisplayName { get; set; }
-
-	/// <summary>
-	/// The type
-	/// </summary>
-	[DataMember(Name = "type")]
-	public string Type { get; set; }
-
-	/// <summary>
-	/// The user permission
-	/// </summary>
-	[DataMember(Name = "userPermission")]
-	public UserPermission UserPermission { get; set; }
 }
+
+///// <summary>
+///// An Alerting Disabled on class
+///// </summary>
+//[DataContract]
+//public class AlertingDisabledOn : IdentifiedItem
+//{
+//	/// <summary>
+//	/// The display name
+//	/// </summary>
+//	[DataMember(Name = "displayName")]
+//	public string DisplayName { get; set; }
+
+//	/// <summary>
+//	/// The type
+//	/// </summary>
+//	[DataMember(Name = "type")]
+//	public string Type { get; set; }
+
+//	/// <summary>
+//	/// The user permission
+//	/// </summary>
+//	[DataMember(Name = "userPermission")]
+//	public UserPermission UserPermission { get; set; }
+//}
