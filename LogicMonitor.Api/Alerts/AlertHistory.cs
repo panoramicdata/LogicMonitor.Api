a72cfc7139745257f6dff40e1d0052e87c7cf04a
andrewhallmark <andrew.hallmark@panoramic.systems>
2022-07-26 12:37:24 +0100

    AM-106 - Add retrieval of alert history

---
+++ LogicMonitor.Api/Alerts/AlertHistory.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicMonitor.Api.Alerts;
+
+/// <summary>
+/// The history associated with an alert
+/// </summary>
+[DataContract]
+public class AlertHistory
+{
+	/// <summary>
+	/// The number of alerts that occurred in the requested period
+	/// </summary>
+	[DataMember(Name = "alertCount")]
+	public int AlertCount { get; set; }
+
+	/// <summary>
+	/// The histogram detailing the distribution of the alerts in the period
+	/// </summary>
+	[DataMember(Name = "histogram")]
+	public Histogram Histogram { get; set; } = null!;
+
+	/// <summary>
+	/// The type of the alert
+	/// </summary>
+	[DataMember(Name = "type")]
+	public string Type { get; set; } = null!;
+}
