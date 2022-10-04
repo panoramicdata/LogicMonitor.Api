8e21b663c76f7eeffc9d605b11fdcf51d26765c7
andrewhallmark <andrew.hallmark@panoramic.systems>
2022-10-04 18:39:02 +0100

    Resolve test failures due to testing methods and behavioural changes

--- LogicMonitor.Api.Test/Alerts/NewAlertTests.cs
+++ LogicMonitor.Api.Test/Alerts/NewAlertTests.cs
@@ -52,8 +52,8 @@ public class NewAlertTests : TestWithOutput
 		(sdtAlerts.Count + nonSdtAlerts.Count).Should().Be(allAlerts.Count);
 
 		// Alerts have the expected SDT status
-		sdtAlerts.Should().AllSatisfy(a => a.InScheduledDownTime.Should().BeTrue());
-		nonSdtAlerts.Should().AllSatisfy(a => a.InScheduledDownTime.Should().BeFalse());
+		Assert.All(sdtAlerts, a => Assert.True(a.InScheduledDownTime));
+		Assert.All(nonSdtAlerts, a => Assert.False(a.InScheduledDownTime));
 	}
 
 	[Fact]
