44382f92d989ae2ff6f02cfb093e7025e492406b
andrewhallmark <andrew.hallmark@panoramic.systems>
2022-10-04 18:39:23 +0100

    Add missing enum members causing test failures

--- LogicMonitor.Api/LogicModules/CollectionMethod.cs
+++ LogicMonitor.Api/LogicModules/CollectionMethod.cs
@@ -17,461 +17,515 @@ public enum CollectionMethod
 	/// Aggregate
 	/// </summary>
 	[EnumMember(Value = "aggregate")]
-	Aggregate,
+	Aggregate = 1,
 
 	/// <summary>
 	/// AWS Billing
 	/// </summary>
 	[EnumMember(Value = "awsbilling")]
-	AwsBilling,
+	AwsBilling = 2,
 
 	/// <summary>
 	/// AWS Billing Report
 	/// </summary>
 	[EnumMember(Value = "awsbillingreport")]
-	AwsBillingReport,
+	AwsBillingReport = 3,
 
 	/// <summary>
 	/// AwsClassicElbServiceLimits
 	/// </summary>
 	[EnumMember(Value = "awsclassicelbservicelimits")]
-	AwsClassicElbServiceLimits,
+	AwsClassicElbServiceLimits = 4,
 
 	/// <summary>
 	/// AWS Cloud watch
 	/// </summary>
 	[EnumMember(Value = "awscloudwatch")]
-	AwsCloudWatch,
+	AwsCloudWatch = 5,
 
 	/// <summary>
 	/// AWS Dynamo DB
 	/// </summary>
 	[EnumMember(Value = "awsdynamodb")]
-	AwsDynamoDb,
+	AwsDynamoDb = 6,
 
 	/// <summary>
 	/// AWS EC2 Reserved Instance
 	/// </summary>
 	[EnumMember(Value = "awsec2reservedinstance")]
-	AwsEc2ReservedInstance,
+	AwsEc2ReservedInstance = 7,
 
 	/// <summary>
 	/// AWS EC2 Reserved Instance Coverage
 	/// </summary>
 	[EnumMember(Value = "awsec2reservedinstancecoverage")]
-	AwsEc2ReservedInstanceCoverage,
+	AwsEc2ReservedInstanceCoverage = 8,
 
 	/// <summary>
 	/// AWS EC2 Service Limits
 	/// </summary>
 	[EnumMember(Value = "awsec2servicelimits")]
-	AwsEc2ServiceLimits,
+	AwsEc2ServiceLimits = 9,
 
 	/// <summary>
 	/// AWS S3
 	/// </summary>
 	[EnumMember(Value = "awss3")]
-	AwsS3,
+	AwsS3 = 10,
 
 	/// <summary>
 	/// AwsServiceLimitsFromTrustedAdvisor
 	/// </summary>
 	[EnumMember(Value = "awsservicelimitsfromtrustedadvisor")]
-	AwsServiceLimitsFromTrustedAdvisor,
+	AwsServiceLimitsFromTrustedAdvisor = 11,
 
 	/// <summary>
 	/// AWS SQS
 	/// </summary>
 	[EnumMember(Value = "awssqs")]
-	AwsSqs,
+	AwsSqs = 12,
 
 	/// <summary>
 	/// AWS Auto Scaling Service Limits
 	/// </summary>
 	[EnumMember(Value = "awsautoscalingservicelimits")]
-	AwsAutoScalingServiceLimits,
+	AwsAutoScalingServiceLimits = 13,
 
 	/// <summary>
 	/// AWS EBS Volumes Snapshot
 	/// </summary>
 	[EnumMember(Value = "awsebsvolumesnapshot")]
-	AwsEbsVolumesSnapshot,
+	AwsEbsVolumesSnapshot = 14,
 
 	/// <summary>
 	/// AWS EC2 Scheduled Events
 	/// </summary>
 	[EnumMember(Value = "awsec2scheduledevents")]
-	AwsEc2ScheduledEvents,
+	AwsEc2ScheduledEvents = 15,
 
 	/// <summary>
 	/// AWS ECS Service Details
 	/// </summary>
 	[EnumMember(Value = "awsecsservicedetails")]
-	AwsEcsServiceDetails,
+	AwsEcsServiceDetails = 16,
 
 	/// <summary>
 	/// AWS RDS Performance Insights
 	/// </summary>
 	[EnumMember(Value = "awsrdsperformanceinsights")]
-	AwsRdsPerformanceInsights,
+	AwsRdsPerformanceInsights = 17,
 
 	/// <summary>
 	/// AWS RDS Service Limits
 	/// </summary>
 	[EnumMember(Value = "awsrdsservicelimits")]
-	AwsRdsServiceLimits,
+	AwsRdsServiceLimits = 18,
 
 	/// <summary>
 	/// AWS SES Service Limits
 	/// </summary>
 	[EnumMember(Value = "awssesservicelimits")]
-	AwsSesServiceLimits,
+	AwsSesServiceLimits = 19,
 
 	/// <summary>
 	/// Azure Automation Account Certificate
 	/// </summary>
 	[EnumMember(Value = "azureautomationaccountcertificate")]
-	AzureAutomationAccountCertificate,
+	AzureAutomationAccountCertificate = 20,
 
 	/// <summary>
 	/// Azure App Service Environment Multi Role Pool
 	/// </summary>
 	[EnumMember(Value = "azureappserviceenvironmentmultirolepool")]
-	AzureAppServiceEnvironmentMultiRolePool,
+	AzureAppServiceEnvironmentMultiRolePool = 21,
 
 	/// <summary>
 	/// Azure express route circuit peering
 	/// </summary>
 	[EnumMember(Value = "azureexpressroutecircuitpeering")]
-	AzureExpressRouteCircuitPeering,
+	AzureExpressRouteCircuitPeering = 22,
 
 	/// <summary>
 	/// Azure replication job
 	/// </summary>
 	[EnumMember(Value = "azurereplicationjob")]
-	AzureReplicationJob,
+	AzureReplicationJob = 23,
 
 	/// <summary>
 	/// Azure backup job
 	/// </summary>
 	[EnumMember(Value = "azurebackupjob")]
-	AzureBackupJob,
+	AzureBackupJob = 24,
 
 	/// <summary>
 	/// Azure Billing
 	/// </summary>
 	[EnumMember(Value = "azurebilling")]
-	AzureBilling,
+	AzureBilling = 25,
 
 	/// <summary>
 	/// Azure Insights
 	/// </summary>
 	[EnumMember(Value = "azureinsights")]
-	AzureInsights,
+	AzureInsights = 26,
 
 	/// <summary>
 	/// AWS Network Service Limits
 	/// </summary>
 	[EnumMember(Value = "azurenetworkservicelimits")]
-	AzureNetworkServiceLimits,
+	AzureNetworkServiceLimits = 27,
 
 	/// <summary>
 	/// Azure Resource Health
 	/// </summary>
 	[EnumMember(Value = "azureresourcehealth")]
-	AzureResourceHealth,
+	AzureResourceHealth = 28,
 
 	/// <summary>
 	/// Azure Virtual Desktop Host Pools
 	/// </summary>
 	[EnumMember(Value = "azurevirtualdesktophostpools")]
-	AzureVirtualDesktopHostPools,
+	AzureVirtualDesktopHostPools = 29,
 
 	/// <summary>
 	/// Azure Virtual Desktop Session Hosts
 	/// </summary>
 	[EnumMember(Value = "azurevirtualdesktopsessionhosts")]
-	AzureVirtualDesktopSessionHosts,
+	AzureVirtualDesktopSessionHosts = 30,
 
 	/// <summary>
 	/// Azure VM Backup Status
 	/// </summary>
 	[EnumMember(Value = "azurevmbackupstatus")]
-	AzureVmBackupStatus,
+	AzureVmBackupStatus = 31,
 
 	/// <summary>
 	/// Azure VNG Connection
 	/// </summary>
 	[EnumMember(Value = "azurevngconnection")]
-	AzureVngConnection,
+	AzureVngConnection = 32,
 
 	/// <summary>
 	/// Azure SDK
 	/// </summary>
 	[EnumMember(Value = "azuresdk")]
-	AzureSdk,
+	AzureSdk = 33,
 
 	/// <summary>
 	/// Azure Storage Service Limits
 	/// </summary>
 	[EnumMember(Value = "azurestorageservicelimits")]
-	AzureStorageServiceLimits,
+	AzureStorageServiceLimits = 34,
 
 	/// <summary>
 	/// Azure VM Service Limits
 	/// </summary>
 	[EnumMember(Value = "azurevmservicelimits")]
-	AzureVmServiceLimits,
+	AzureVmServiceLimits = 35,
 
 	/// <summary>
 	/// Azure Web Job
 	/// </summary>
 	[EnumMember(Value = "azurewebjob")]
-	AzureWebJob,
+	AzureWebJob = 36,
 
 	/// <summary>
 	/// AWS Cloud watch
 	/// </summary>
 	[EnumMember(Value = "batchscript")]
-	Batchscript,
+	Batchscript = 37,
 
 	/// <summary>
 	/// AWS Cloud watch
 	/// </summary>
 	[EnumMember(Value = "cim")]
-	Cim,
+	Cim = 38,
 
 	/// <summary>
 	/// Datapump
 	/// </summary>
 	[EnumMember(Value = "datapump")]
-	Datapump,
+	Datapump = 39,
 
 	/// <summary>
 	/// DNS
 	/// </summary>
 	[EnumMember(Value = "dns")]
-	Dns,
+	Dns = 40,
 
 	/// <summary>
 	/// ESX
 	/// </summary>
 	[EnumMember(Value = "esx")]
-	Esx,
+	Esx = 41,
 
 	/// <summary>
 	/// GCP Billing
 	/// </summary>
 	[EnumMember(Value = "gcpbilling")]
-	GcpBilling,
+	GcpBilling = 42,
 
 	/// <summary>
 	/// GCP Billing Big Query
 	/// </summary>
 	[EnumMember(Value = "gcpbillingbigquery")]
-	GcpBillingBigQuery,
+	GcpBillingBigQuery = 43,
 
 	/// <summary>
 	/// GCP Compute Service Limits
 	/// </summary>
 	[EnumMember(Value = "gcpcomputeservicelimits")]
-	GcpComputeServiceLimits,
+	GcpComputeServiceLimits = 44,
 
 	/// <summary>
 	/// GCP Stack Driver
 	/// </summary>
 	[EnumMember(Value = "gcpstackdriver")]
-	GcpStackDriver,
+	GcpStackDriver = 45,
 
 	/// <summary>
 	/// GCP Back-end Service Health
 	/// </summary>
 	[EnumMember(Value = "gcpbackendservicehealth")]
-	GcpBackendServiceHealth,
+	GcpBackendServiceHealth = 46,
 
 	/// <summary>
 	/// Internal
 	/// </summary>
 	[EnumMember(Value = "internal")]
-	Internal,
+	Internal = 47,
 
 	/// <summary>
 	/// Internal
 	/// </summary>
 	[EnumMember(Value = "ipmi")]
-	Ipmi,
+	Ipmi = 48,
 
 	/// <summary>
 	/// JDBC
 	/// </summary>
 	[EnumMember(Value = "jdbc")]
-	Jdbc,
+	Jdbc = 49,
 
 	/// <summary>
 	/// JMX
 	/// </summary>
 	[EnumMember(Value = "jmx")]
-	Jmx,
+	Jmx = 50,
 
 	/// <summary>
 	/// Memcached
 	/// </summary>
 	[EnumMember(Value = "memcached")]
-	Memcached,
+	Memcached = 51,
 
 	/// <summary>
 	/// AWS Cloud watch
 	/// </summary>
 	[EnumMember(Value = "mongo")]
-	Mongo,
+	Mongo = 52,
 
 	/// <summary>
 	/// Netapp
 	/// </summary>
 	[EnumMember(Value = "netapp")]
-	Netapp,
+	Netapp = 53,
 
 	/// <summary>
 	/// Openmetrics
 	/// </summary>
 	[EnumMember(Value = "openmetrics")]
-	OpenMetrics,
+	OpenMetrics = 54,
 
 	/// <summary>
 	/// Perfmon
 	/// </summary>
 	[EnumMember(Value = "perfmon")]
-	Perfmon,
+	Perfmon = 55,
 
 	/// <summary>
 	/// Ping
 	/// </summary>
 	[EnumMember(Value = "ping")]
-	Ping,
+	Ping = 56,
 
 	/// <summary>
 	/// Property
 	/// </summary>
 	[EnumMember(Value = "property")]
-	Property,
+	Property = 57,
 
 	/// <summary>
 	/// pushmodules
 	/// </summary>
 	[EnumMember(Value = "pushmodules")]
-	PushModules,
+	PushModules = 58,
 
 	/// <summary>
 	/// saas airbrake
 	/// </summary>
 	[EnumMember(Value = "saasairbrake")]
-	SaasAirbrake,
+	SaasAirbrake = 59,
 
 	/// <summary>
 	/// saasoffice365csvreport
 	/// </summary>
 	[EnumMember(Value = "saasoffice365csvreport")]
-	SaasOffice365CsvReport,
+	SaasOffice365CsvReport = 60,
 
 	/// <summary>
 	/// saasoffice365sharepointreport
 	/// </summary>
 	[EnumMember(Value = "saasoffice365sharepointreport")]
-	SaasOffice365SharepointReport,
+	SaasOffice365SharepointReport = 61,
 
 	/// <summary>
 	/// saasoffice365subscribedskus
 	/// </summary>
 	[EnumMember(Value = "saasoffice365subscribedskus")]
-	Saasoffice365SubscribedSkus,
+	Saasoffice365SubscribedSkus = 62,
 
 	/// <summary>
 	/// saassalesforcejson
 	/// </summary>
 	[EnumMember(Value = "saassalesforcejson")]
-	SaasSalesforceJson,
+	SaasSalesforceJson = 63,
 
 	/// <summary>
 	/// saassalesforcesoqlquery
 	/// </summary>
 	[EnumMember(Value = "saassalesforcesoqlquery")]
-	SaasSalesforceSoqlQuery,
+	SaasSalesforceSoqlQuery = 64,
 
 	/// <summary>
 	/// saas status
 	/// </summary>
 	[EnumMember(Value = "saasstatus")]
-	SaasStatus,
+	SaasStatus = 65,
 
 	/// <summary>
 	/// saas zoom json
 	/// </summary>
 	[EnumMember(Value = "saaszoomjson")]
-	SaasZoomJson,
+	SaasZoomJson = 66,
 
 	/// <summary>
 	/// saas zoom plan usage
 	/// </summary>
 	[EnumMember(Value = "saaszoomplanusage")]
-	SaasZoomPlanUsage,
+	SaasZoomPlanUsage = 67,
 
 	/// <summary>
 	/// script
 	/// </summary>
 	[EnumMember(Value = "script")]
-	Script,
+	Script = 68,
 
 	/// <summary>
 	/// Script
 	/// </summary>
 	[EnumMember(Value = "Script")]
-	Script2,
+	Script2 = 69,
 
 	/// <summary>
 	/// Script
 	/// </summary>
 	[EnumMember(Value = "syntheticsselenium")]
-	SyntheticsSelenium,
+	SyntheticsSelenium = 70,
 
 	/// <summary>
 	/// SNMP
 	/// </summary>
 	[EnumMember(Value = "snmp")]
-	Snmp,
+	Snmp = 71,
 
 	/// <summary>
 	/// TCP
 	/// </summary>
 	[EnumMember(Value = "tcp")]
-	Tcp,
+	Tcp = 72,
 
 	/// <summary>
 	/// UDP
 	/// </summary>
 	[EnumMember(Value = "udp")]
-	Udp,
+	Udp = 73,
 
 	/// <summary>
 	/// WMI
 	/// </summary>
 	[EnumMember(Value = "wmi")]
-	Wmi,
+	Wmi = 74,
 
 	/// <summary>
 	/// Webpage
 	/// </summary>
 	[EnumMember(Value = "webpage")]
-	Webpage,
+	Webpage = 75,
 
 	/// <summary>
 	/// Webpage (different spelling of property in the JSON!)
 	/// </summary>
 	[EnumMember(Value = "Webpage")] // version 136 introduced this issue!
-	Webpage1,
+	Webpage1 = 76,
 
 	/// <summary>
 	/// Xen
 	/// </summary>
 	[EnumMember(Value = "xen")]
-	Xen
+	Xen = 77,
+
+	/// <summary>
+	/// Azure backup protected item backup job
+	/// </summary>
+	[EnumMember(Value = "azurebackupprotecteditembackupjob")]
+	AzureBackupProtectedItemBackupJob = 101,
+
+	/// <summary>
+	/// Azure backup protected item health
+	/// </summary>
+	[EnumMember(Value = "azurebackupprotecteditemhealth")]
+	AzureBackupProtectedItemHealth = 102,
+
+	/// <summary>
+	/// Azure cost management
+	/// </summary>
+	[EnumMember(Value = "azurecostmanagement")]
+	AzureCostManagement = 103,
+
+	/// <summary>
+	/// Azure log analytics replication job
+	/// </summary>
+	[EnumMember(Value = "azureloganalyticsreplicationjob")]
+	AzureLogAnalyticsReplicationJob = 104,
+
+	/// <summary>
+	/// Azure log analytics workspaces
+	/// </summary>
+	[EnumMember(Value = "azureloganalyticsworkspaces")]
+	AzureLogAnalyticsWorkspaces = 105,
+
+	/// <summary>
+	/// Azure recovery service vault sr
+	/// </summary>
+	[EnumMember(Value = "azurerecoveryservicevaultsr")]
+	AzureRecoveryServiceVaultSR = 106,
+
+	/// <summary>
+	/// Azure replication disaster recovery
+	/// </summary>
+	[EnumMember(Value = "azurereplicationdisasterrecovery")]
+	AzureReplicationDisasterRecovery = 107,
+
+	/// <summary>
+	/// SASS Office 365 service health
+	/// </summary>
+	[EnumMember(Value = "saasoffice365servicehealth")]
+	SaasOffice365ServiceHealth = 201,
+
+	/// <summary>
+	/// SASS Salesforce instance status
+	/// </summary>
+	[EnumMember(Value = "saassalesforceinstancestatus")]
+	SaasSalesforceInstanceStatus = 202,
 }
