8dd39429aa7391789651c1e5cdb69183cabc9890
geckogeckogecko <lorna.harvey@panoramicdata.com>
2023-01-24 14:49:40 +0000

    Added necessary Data and Datapoint models.

---
+++ LogicMonitor.Api/Dashboards/OverviewGraphDataPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicMonitor.Api.Dashboards;
+
+/// <summary>
+/// OverviewGraphDataPoint
+/// </summary>
+
+[DataContract]
+public class OverviewGraphDataPoint
+{
+	/// <summary>
+	/// the graph line data point aggregate method, average|min|max|sum
+	/// </summary>
+	[DataMember(Name = "aggregateMethod")]
+	public string? AggregateMethod { get; set; }
+
+	/// <summary>
+	/// the graph line data point name
+	/// </summary>
+	[DataMember(Name = "dataPointName")]
+	public string? DataPointName { get; set; }
+
+	/// <summary>
+	/// the graph line data point id
+	/// </summary>
+	[DataMember(Name = "dataPointId")]
+	public int DataPointId { get; set; }
+
+	/// <summary>
+	/// the graph line data point consolidate function, 1\u003davg|2\u003dmax|3\u003dmin
+	/// </summary>
+	[DataMember(Name = "consolidateFunc")]
+	public int ConsolidateFunc { get; set; }
+
+	/// <summary>
+	/// dataSourceDataPointId
+	/// </summary>
+	[DataMember(Name = "dataSourceDataPointId")]
+	public int DataSourceDataPointId { get; set; }
+}
--- LogicMonitor.Api/LogicModules/DataSourceDataPoint.cs
+++ LogicMonitor.Api/LogicModules/DataSourceDataPoint.cs
@@ -7,31 +7,31 @@ namespace LogicMonitor.Api.LogicModules;
 public class DataSourceDataPoint : NamedItem
 {
 	/// <summary>
-	/// The alertExprNote
+	/// alert expression note
 	/// </summary>
 	[DataMember(Name = "alertExprNote")]
-	public string AlertExpressionNote { get; set; }
+	public string? AlertExpressionNote { get; set; }
 
 	/// <summary>
-	/// The DataPoint Id
+	/// The datapoint id
 	/// </summary>
-	[DataMember(Name = "dataPointId")]
+	[DataMember(Name = "id")]
 	public int DataPointId { get; set; }
 
 	/// <summary>
-	/// The DataSource Id
+	/// The datasource id
 	/// </summary>
 	[DataMember(Name = "dataSourceId")]
 	public int DataSourceId { get; set; }
 
 	/// <summary>
-	/// alertTransitionInterval
+	/// The count that the alert must exist for this many poll cycles before it will be triggered
 	/// </summary>
 	[DataMember(Name = "alertTransitionInterval")]
 	public int AlertTransitionInterval { get; set; }
 
 	/// <summary>
-	/// alertClearTransitionInterval
+	/// The count that the alert must exist for this many poll cycles before the alert will be cleared
 	/// </summary>
 	[DataMember(Name = "alertClearTransitionInterval")]
 	public int AlertClearTransitionInterval { get; set; }
@@ -40,131 +40,131 @@ public class DataSourceDataPoint : NamedItem
 	/// The consolidation function
 	/// </summary>
 	[DataMember(Name = "consolidateFunc")]
-	public string ConsolidationFunction { get; set; }
+	public string? ConsolidationFunction { get; set; }
 
 	/// <summary>
-	/// type
+	/// The data metric type, values can be 0-7 (0:unknown, 1:counter, 2:gauge, 3:derive, 5:status, 6:compute, 7:counter32, 8:counter64)
 	/// </summary>
 	[DataMember(Name = "type")]
 	public int Type { get; set; }
 
 	/// <summary>
-	/// dataType
+	/// The data value type, values can be 1-8 (1:boolean, 2:byte, 3:short, 4:int, 5:long, 6:float, 7:double, 8:ulong)
 	/// </summary>
 	[DataMember(Name = "dataType")]
 	public int DataType { get; set; }
 
 	/// <summary>
-	/// maxDigits
+	/// The max digits of the data value
 	/// </summary>
 	[DataMember(Name = "maxDigits")]
 	public int MaxDigits { get; set; }
 
 	/// <summary>
-	/// postProcessorMethod
+	/// The post processor method for the data value. Currently support complex expression and groovy.
 	/// </summary>
 	[DataMember(Name = "postProcessorMethod")]
-	public string PostProcessorMethod { get; set; }
+	public string? PostProcessorMethod { get; set; }
 
 	/// <summary>
-	/// postProcessorMethod
+	/// The post processor parameter, e.g. dataPoint1*2
 	/// </summary>
 	[DataMember(Name = "postProcessorParam")]
-	public string PostProcessorParam { get; set; }
+	public string? PostProcessorParam { get; set; }
 
 	/// <summary>
-	/// rawDataFieldName
+	/// The name of the raw data field name used to fetch value, e.g. avgrtt, output
 	/// </summary>
 	[DataMember(Name = "rawDataFieldName")]
-	public string RawDataFieldName { get; set; }
+	public string? RawDataFieldName { get; set; }
 
 	/// <summary>
-	/// maxValue
+	/// The max value of the datapoint value range
 	/// </summary>
 	[DataMember(Name = "maxValue")]
-	public string MaxValue { get; set; }
+	public string? MaxValue { get; set; }
 
 	/// <summary>
-	/// minValue
+	/// The minimum value of the datapoint value range
 	/// </summary>
 	[DataMember(Name = "minValue")]
-	public string MinValue { get; set; }
+	public string? MinValue { get; set; }
 
 	/// <summary>
-	/// userParam1
+	/// The first user parameter will be used to fetch the datapoint value. e.g. snmp oid
 	/// </summary>
 	[DataMember(Name = "userParam1")]
-	public string UserParam1 { get; set; }
+	public string? UserParam1 { get; set; }
 
 	/// <summary>
-	/// userParam2
+	/// The second user parameter will be used to fetch the datapoint value. e.g. jmx attribute name
 	/// </summary>
 	[DataMember(Name = "userParam2")]
-	public string UserParam2 { get; set; }
+	public string? UserParam2 { get; set; }
 
 	/// <summary>
-	/// userParam3
+	/// The third user parameter will be used to fetch the datapoint value. 
 	/// </summary>
 	[DataMember(Name = "userParam3")]
-	public string UserParam3 { get; set; }
+	public string? UserParam3 { get; set; }
 
 	/// <summary>
-	/// alertForNoData
+	/// The triggered alert level if we cannot collect data for this datapoint, value can be 1-4 (0:unused alert, 1:alert ok, 2:warn alert, 2:error alert, 4:critical alert)
 	/// </summary>
 	[DataMember(Name = "alertForNoData")]
 	public int AlertForNoData { get; set; }
 
 	/// <summary>
-	/// The alert expression
+	/// The alert threshold define for the datapoint. e.g. \u0027\u003e 60 80 90\u0027 mean it will: \ntrigger warn alert if value \u003e 60\ntrigger error alert if value \u003e 80\ntrigger critical alert if value \u003e 90
 	/// </summary>
 	[DataMember(Name = "alertExpr")]
-	public string AlertExpression { get; set; }
+	public string? AlertExpression { get; set; }
 
 	/// <summary>
-	/// alertSubject
+	/// The customized alert message subject define, empty string mean we will use the define in default template
 	/// </summary>
 	[DataMember(Name = "alertSubject")]
-	public string AlertSubject { get; set; }
+	public string? AlertSubject { get; set; }
 
 	/// <summary>
-	/// alertBody
+	/// The customized alert message body define,  empty string mean we will use the define in default template
 	/// </summary>
 	[DataMember(Name = "alertBody")]
-	public string AlertBody { get; set; }
+	public string? AlertBody { get; set; }
 
 	/// <summary>
-	/// Suppress anomaly alerts
+	/// Expression of anomaly detection setting, split by comma\n0 means off,  1 means on, -1 means invalid\n1,0,1 \u003d   warn : ON     error: OFF   critical: ON\nEmpty value on this parameter means : 0,0,0
 	/// </summary>
 	[DataMember(Name = "enableAnomalyAlertSuppression")]
 	public string? SuppressAnomalyAlerts { get; set; }
 
 	/// <summary>
-	/// adAdvSettingEnabled
+	/// the AD advance setting enable flag
 	/// </summary>
 	[DataMember(Name = "adAdvSettingEnabled")]
 	public bool IsAdAdvSettingEnabled { get; set; }
 
 	/// <summary>
-	/// warnAdAdvSetting
+	/// enable anomaly detection advance setting for WARN severity
 	/// </summary>
 	[DataMember(Name = "warnAdAdvSetting")]
-	public string WarnAdAdvSetting { get; set; }
+	public string? WarnAdAdvSetting { get; set; }
 
 	/// <summary>
-	/// errorAdAdvSetting
+	/// enable anomaly detection advance setting for ERROR severity
 	/// </summary>
 	[DataMember(Name = "errorAdAdvSetting")]
-	public string ErrorAdAdvSetting { get; set; }
+	public string? ErrorAdAdvSetting { get; set; }
 
 	/// <summary>
-	/// criticalAdAdvSetting
+	/// enable anomaly detection advance setting for CRITICAL severity
 	/// </summary>
 	[DataMember(Name = "criticalAdAdvSetting")]
-	public string CriticialAdAdvSetting { get; set; }
+	public string? CriticialAdAdvSetting { get; set; }
 
 	/// <summary>
-	/// Origin id
+	/// portable id for origin tracking
 	/// </summary>
 	[DataMember(Name = "originId")]
-	public string OriginId { get; set; }
+	public string? OriginId { get; set; }
 }
