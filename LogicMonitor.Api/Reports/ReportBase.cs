1b7c5672a6bca9a9079886bc729e98fb7242b941
geckogeckogecko <lorna.harvey@panoramicdata.com>
2023-02-12 15:31:00 +0000

    Updated Settings models.

--- LogicMonitor.Api/Reports/ReportBase.cs
+++ LogicMonitor.Api/Reports/ReportBase.cs
@@ -9,117 +9,117 @@ namespace LogicMonitor.Api.Reports;
 public class ReportBase : NamedItem, IHasEndpoint
 {
 	/// <summary>
-	/// The report type
+	/// The report type. Acceptable values are: Alert,Alert SLA,Alert threshold,Alert trends,Host CPU,Host group inventory,Host inventory,Host metric trends,Interfaces Bandwidth,Netflow device metric,Service Level Agreement,Website Service Overview,Word template,Audit Log,Alert Forecasting,Dashboard,Website SLA,User,Role
 	/// </summary>
-	[DataMember(Name = "type")]
-	public string Type { get; set; }
+	[DataMember(Name = "type", IsRequired = true)]
+	public string Type { get; set; } = null!;
 
 	/// <summary>
 	/// The type alias
 	/// </summary>
-	[DataMember(Name = "typeAlias")]
-	public string TypeAlias { get; set; }
+	[DataMember(Name = "typeAlias", IsRequired = false)]
+	public string? TypeAlias { get; set; }
 
 	/// <summary>
-	/// The group id
+	/// The Id of the group the report is in, where Id\u003d0 is the root report group
 	/// </summary>
-	[DataMember(Name = "groupId")]
+	[DataMember(Name = "groupId", IsRequired = false)]
 	public int GroupId { get; set; }
 
 	/// <summary>
-	/// The format
+	/// The format of the report. Allowable values are: HTML, PDF, CSV, WORD
 	/// </summary>
-	[DataMember(Name = "format")]
-	public string Format { get; set; }
+	[DataMember(Name = "format", IsRequired = false)]
+	public string? Format { get; set; }
 
 	/// <summary>
-	/// The delivery
+	/// Whether or not the report is configured to be delivered via email. Acceptable values are: none, email
 	/// </summary>
-	[DataMember(Name = "delivery")]
-	public string Delivery { get; set; }
+	[DataMember(Name = "delivery", IsRequired = false)]
+	public string? Delivery { get; set; }
 
 	/// <summary>
-	/// The recipients
+	/// If the report is configured to be delivered via email, this object provides the recipients that the report will be delivered to
 	/// </summary>
-	[DataMember(Name = "recipients")]
-	public List<Recipient> Recipients { get; set; }
+	[DataMember(Name = "recipients", IsRequired = false)]
+	public List<Recipient>? Recipients { get; set; }
 
 	/// <summary>
-	/// The schedule
+	/// A cron schedule that indicates when the report will be delivered via email
 	/// </summary>
-	[DataMember(Name = "schedule")]
-	public string Schedule { get; set; }
+	[DataMember(Name = "schedule", IsRequired = false)]
+	public string? Schedule { get; set; }
 
 	/// <summary>
-	/// The schedule time zone
+	/// The specific timezone for the scheduled report
 	/// </summary>
-	[DataMember(Name = "scheduleTimezone")]
-	public string ScheduleTimezone { get; set; }
+	[DataMember(Name = "scheduleTimezone", IsRequired = false)]
+	public string? ScheduleTimezone { get; set; }
 
 	/// <summary>
-	/// The id of the user to last modify it
+	/// The Id of the user that last modified the report
 	/// </summary>
-	[DataMember(Name = "lastmodifyUserId")]
+	[DataMember(Name = "lastmodifyUserId", IsRequired = false)]
 	public int LastmodifyUserId { get; set; }
 
 	/// <summary>
-	/// The username of the user to last modify it
+	/// The username of the user that last modified the report
 	/// </summary>
-	[DataMember(Name = "lastmodifyUserName")]
-	public string LastmodifyUserName { get; set; }
+	[DataMember(Name = "lastmodifyUserName", IsRequired = false)]
+	public string? LastmodifyUserName { get; set; }
 
 	/// <summary>
-	/// Whether to enable view as another user
+	/// Whether or not other users are allowed to view the report as the user who last modified the report
 	/// </summary>
-	[DataMember(Name = "enableViewAsOtherUser")]
+	[DataMember(Name = "enableViewAsOtherUser", IsRequired = false)]
 	public bool EnableViewAsOtherUser { get; set; }
 
 	/// <summary>
-	/// User permission
+	/// The permissions associated with the user who made the API call
 	/// </summary>
-	[DataMember(Name = "userPermission")]
+	[DataMember(Name = "userPermission", IsRequired = false)]
 	public UserPermission UserPermission { get; set; }
 
 	/// <summary>
-	/// The last generation time
+	/// The time, in epoch format, that the report was last generated
 	/// </summary>
-	[DataMember(Name = "lastGenerateOn")]
-	public int LastGenerateOn { get; set; }
+	[DataMember(Name = "lastGenerateOn", IsRequired = false)]
+	public long LastGenerateOn { get; set; }
 
 	/// <summary>
-	/// The size the last time it was generated
+	/// The size of the report, in Bytes, the last time it was generated
 	/// </summary>
-	[DataMember(Name = "lastGenerateSize")]
-	public int LastGenerateSize { get; set; }
+	[DataMember(Name = "lastGenerateSize", IsRequired = false)]
+	public long LastGenerateSize { get; set; }
 
 	/// <summary>
-	/// The number of pages last time it was generated
+	/// The number of pages in the report, the last time it was generated
 	/// </summary>
-	[DataMember(Name = "lastGeneratePages")]
+	[DataMember(Name = "lastGeneratePages", IsRequired = false)]
 	public int LastGeneratePages { get; set; }
 
 	/// <summary>
-	/// The custom report type id
+	/// The id of the custom report template, if the report is a custom report. An id of 0 indicates that the report is not a custom report
 	/// </summary>
-	[DataMember(Name = "customReportTypeId")]
+	[DataMember(Name = "customReportTypeId", IsRequired = false)]
 	public int CustomReportTypeId { get; set; }
 
 	/// <summary>
-	/// The custom report type name
+	/// The name of the custom report template
 	/// </summary>
-	[DataMember(Name = "customReportTypeName")]
-	public string CustomReportTypeName { get; set; }
+	[DataMember(Name = "customReportTypeName", IsRequired = false)]
+	public string? CustomReportTypeName { get; set; }
 
 	/// <summary>
-	/// When the report Link expires
+	/// The report link Expire. Allowable values are:High Flexibility,High Security
 	/// </summary>
-	[DataMember(Name = "reportLinkExpire")]
-	public string ReportLinkExpire { get; set; }
+	[DataMember(Name = "reportLinkExpire", IsRequired = false)]
+	public string? ReportLinkExpire { get; set; }
 
 	/// <summary>
-	/// The report link number
+	/// The number of links associated with the report, where each link corresponds to a generated report
 	/// </summary>
-	[DataMember(Name = "reportLinkNum")]
+	[DataMember(Name = "reportLinkNum", IsRequired = false)]
 	public int ReportLinkNum { get; set; }
 
 	/// <summary>
--- LogicMonitor.Api/Reports/SlaReport.cs
+++ LogicMonitor.Api/Reports/SlaReport.cs
@@ -7,56 +7,56 @@ namespace LogicMonitor.Api.Reports;
 public class SlaReport : DateRangeReport
 {
 	/// <summary>
-	/// The calculation method
+	/// Calculation method: 0 \u003d percent all resources available, 1 \u003d average of all SLA metrics
 	/// </summary>
-	[DataMember(Name = "calculationMethod")]
+	[DataMember(Name = "calculationMethod", IsRequired = false)]
 	public CalculationMethod CalculationMethod { get; set; }
 
 	/// <summary>
-	/// The dayInOneWeek
+	/// The days of the week that the SLA report should take into account, where multiple values are separated by commas and * refers to all days of the week
 	/// </summary>
-	[DataMember(Name = "dayInOneWeek")]
-	public string DayInOneWeek { get; set; }
+	[DataMember(Name = "dayInOneWeek", IsRequired = false)]
+	public string? DayInOneWeek { get; set; }
 
 	/// <summary>
 	/// The metrics
 	/// </summary>
-	[DataMember(Name = "metrics")]
-	public List<SlaReportMetric> Metrics { get; set; }
+	[DataMember(Name = "metrics", IsRequired = true)]
+	public List<SlaReportMetric> Metrics { get; set; } = null!;
 
 	/// <summary>
-	/// The periodInOneDay
+	/// The hours of each selected day that the SLA report should take into account, where * refers to all hours
 	/// </summary>
-	[DataMember(Name = "periodInOneDay")]
-	public string PeriodInOneDay { get; set; }
+	[DataMember(Name = "periodInOneDay", IsRequired = false)]
+	public string? PeriodInOneDay { get; set; }
 
 	/// <summary>
-	/// The timezone
+	/// The specific timezone for the report
 	/// </summary>
-	[DataMember(Name = "timezone")]
-	public string TimeZone { get; set; }
+	[DataMember(Name = "timezone", IsRequired = false)]
+	public string? TimeZone { get; set; }
 
 	/// <summary>
-	/// The displaySummary
+	/// If true, the SLA summary (total %) will be displayed
 	/// </summary>
-	[DataMember(Name = "displaySummary")]
+	[DataMember(Name = "displaySummary", IsRequired = false)]
 	public bool DisplaySummary { get; set; }
 
 	/// <summary>
-	/// The unmonitoredTime
+	/// 0|1|2 - How the time we have no data for the device should be counted, where 1 \u003d ignore no data (subtract from total time), 2 \u003d count as violation (subtract from uptime), 3 \u003d count as available (add to uptime)
 	/// </summary>
-	[DataMember(Name = "unmonitoredTime")]
+	[DataMember(Name = "unmonitoredTime", IsRequired = false)]
 	public int UnmonitoredTime { get; set; }
 
 	/// <summary>
-	/// The displayWithAvailability
+	/// If true, only devices with less than 100% availability will be displayed in the report
 	/// </summary>
-	[DataMember(Name = "displayWithAvailability")]
+	[DataMember(Name = "displayWithAvailability", IsRequired = false)]
 	public bool DisplayWithAvailability { get; set; }
 
 	/// <summary>
-	/// The columns
+	/// The columns displayed in the report
 	/// </summary>
-	[DataMember(Name = "columns")]
-	public List<ReportColumn> Columns { get; set; }
+	[DataMember(Name = "columns", IsRequired = false)]
+	public List<ReportColumn>? Columns { get; set; }
 }
