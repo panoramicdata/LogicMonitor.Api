1e52b7db414b9de5e86c7681d96064b4f72d96f2
andrewhallmark <46025270+TheCakeMonster@users.noreply.github.com>
2023-03-29 16:08:23 +0100

    LOG-23 - Resolve nullability warning

--- LogicMonitor.Api.Test/Data/DataTests.cs
+++ LogicMonitor.Api.Test/Data/DataTests.cs
@@ -272,7 +272,10 @@ public class DataTests : TestWithOutput
 	public async Task GetDataSourceUpdateReasons()
 	{
 		var dataSource = await LogicMonitorClient.GetDataSourceByUniqueNameAsync("WinCPU", default).ConfigureAwait(false);
-		var updateReasons = await LogicMonitorClient.GetDataSourceUpdateReasonAsync(dataSource.Id, new Filter<DataSourceUpdateReason>(), default).ConfigureAwait(false);
+		dataSource.Should().NotBeNull();
+
+		var updateReasons = await LogicMonitorClient.GetDataSourceUpdateReasonAsync(dataSource!.Id, new Filter<DataSourceUpdateReason>(), default).ConfigureAwait(false);
+
 		updateReasons.Should().NotBeNull();
 		updateReasons.Items.Should().NotBeEmpty();
 	}
