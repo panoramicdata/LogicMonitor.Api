1a7393ed5147b0c765be65f2ea3178bbbbab60f8
David Bond <david.bond@panoramicdata.com>
2023-12-07 19:09:35 +0000

    IDE0022

--- LogicMonitor.Api.Test/LogicModules/TopologySourceTests.cs
+++ LogicMonitor.Api.Test/LogicModules/TopologySourceTests.cs
@@ -3,10 +3,6 @@ namespace LogicMonitor.Api.Test.LogicModules;
 public class TopologySourceTests(ITestOutputHelper iTestOutputHelper) : TestWithOutput(iTestOutputHelper)
 {
 	[Fact]
-	public async Task BasicTopologySourceTest()
-	{
-		_ = await LogicMonitorClient
-			.GetAllAsync<TopologySource>(default)
-			.ConfigureAwait(true);
-	}
+	public Task BasicTopologySourceTest() =>
+		_ = LogicMonitorClient.GetAllAsync<TopologySource>(default);
 }
