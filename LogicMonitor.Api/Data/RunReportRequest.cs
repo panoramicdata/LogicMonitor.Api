a3d24fb03a1f2f583555ec4df7650e66282d5878
Roland Banks <roland.banks@panoramicdata.com>
2024-01-24 16:08:54 +0700

    Added Report Creation DTO, and the RunReportById client method.

---
+++ LogicMonitor.Api/Data/RunReportRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicMonitor.Api.Data;
+
+/// <summary>
+/// A run report request
+/// </summary>
+[DataContract]
+public class RunReportRequest
+{
+	/// <summary>
+	/// The With Admin ID
+	/// </summary>
+	[DataMember(Name = "withAdminId")]
+	public int WithAdminId { get; set; } = 0;
+}
