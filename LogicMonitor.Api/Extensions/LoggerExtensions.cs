d2d723ae2772c3132a3d3e8e24614d4326892219
Roland Banks <roland.banks@panoramicdata.com>
2024-03-11 15:29:08 +0700

    LogHttpHeaders now logs "EMPTY" when there are none.

--- LogicMonitor.Api/Extensions/LoggerExtensions.cs
+++ LogicMonitor.Api/Extensions/LoggerExtensions.cs
@@ -14,7 +14,7 @@ public static class LoggerExtensions
 	/// <param name="headers">HTTP headers</param>
 	public static void LogHttpHeaders(this ILogger logger, bool isRequest, string prefix, HttpHeaders? headers)
 	{
-		var guidPrefix = prefix != null ? (prefix + " ") : string.Empty;
+		var guidPrefix = string.IsNullOrWhiteSpace(prefix) ? (prefix + " ") : string.Empty;
 
 		// Log headers
 		if (headers != null)
@@ -27,6 +27,11 @@ public static class LoggerExtensions
 				foreach (var header in headers.Where(x => x.Key != "Authorization"))
 				{
 					output.Append($"HEADER-NAME: {header.Key} | HEADER-VALUE(S): {string.Join(";", header.Value)} \r\n");
+				}
+
+				if (output.Length == 0)
+				{
+					output.Append("EMPTY");
 				}
 
 				logger.LogDebug("{Guid}{HeaderType}{Headers}", guidPrefix, headerType, output.ToString());
